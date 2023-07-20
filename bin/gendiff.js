c60e32f6878705667cbd6f26d068fc75786c61b0
aleksakokorewa <134208188+aleksakokorewa@users.noreply.github.com>
2023-07-20 21:11:16 +0300

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,7 +7,8 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format')
   .helpOption('-h, --help', 'output usage information')
-  .option('-f, --format <type>', 'output format');
+  .arguments('<filepath1> <filepath2>');
 
 program.parse();
